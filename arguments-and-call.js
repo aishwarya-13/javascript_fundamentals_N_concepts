278e2f528bbb8efd80ecb54da1cb549e80f25400
Aishwarya Vhatkar <iaishwaryavhatkar@gmail.com>
2023-04-29 21:46:20 -0700

    arguments example

---
+++ arguments-and-call.js
@@ -0,0 +1,21 @@
+/**
+ * Write a simple function to tell whether 2 is passed as parameter or not?
+ * 
+ * arguments is a local variable, available inside all functions that provides a collection of all the arguments passed to the function.
+ * arguments is not an array rather an array like object. It has length but doesn't have the methods like forEach, indexOf, etc.
+ * First convert arguments to an array by calling slice method on an array and pass arguments. After that simply use indexOf.
+ */
+
+function isTwoPassed(){
+    console.log(arguments);
+    const args = Array.prototype.slice.call(arguments);
+    return args.indexOf(2) !== -1;
+};
+
+console.log(isTwoPassed(1,2));//true
+console.log(isTwoPassed(3,4,5,6));//false
+console.log(isTwoPassed(2));//true
+
+/**
+ * Danger: Don't name any argument as "arguments" or dont create any local variable named as "arguments", this will override build in arguments object.
+ */
