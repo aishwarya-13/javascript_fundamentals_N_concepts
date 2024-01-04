125eba7319eced43b0af90b44e3250e709ad394c
aishwarya vhatkar <iaishwaryavhatkar@gmail.com>
2024-01-03 18:36:56 -0800

    Dynamic typing

---
+++ dynamic_typing.js
@@ -0,0 +1,8 @@
+/**
+ * Dynamic Typing: You do not tell the engine what type of data a variable holds, it figures it out while your code is running
+ * In below example JS will assign a Boolean value to the isNew variable. In the next line of execution the variable will be of type string. In the next line of execution the variable will be a number
+ */
+
+let isNew = true
+isNew = 'i am string'
+isNew = 2
