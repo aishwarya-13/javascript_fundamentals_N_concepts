8ffc593db84af94fe7232c6fdaad4b6e39e507f8
Aishwarya Vhatkar <iaishwaryavhatkar@gmail.com>
2024-01-15 19:35:13 -0800

    basic promise

--- promises/basic_promise.js
+++ promises/basic_promise.js
@@ -1,20 +1,24 @@
+function myPromise(){
+  //1. Create a promise
+  let myPromise = new Promise((resolve, reject)=>{
+    //simulate a asynchronous event
+    setTimeout(()=>{
+      //if success, resolve the promise
+      let success = true
+      if(success){
+        resolve('Data fetch is successfull!')
+      }else{
+        reject('Error is data fetching!')
+      }
+    }, 1000)//simulate a 2-second delay
+  })
+  return myPromise
+}
 
-//1. Create a promise
-let myPromise = new Promise((resolve, reject)=>{
-  //simulate a asynchronous event
-  setTimeout(()=>{
-    //if success, resolve the promise
-    let success = true
-    if(success){
-      resolve('Data fetch is successfull!')
-    }else{
-      reject('Error is data fetching!')
-    }
-  }, 2000)//simulate a 2-second delay
-})
+
 
 //2. Using the promise
-myPromise.then((result)=>{
+myPromise().then((result)=>{
   //this will execute if promise is resolved
   console.log('promise result', result)
 }).catch((error)=>{
