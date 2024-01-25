c91cc7d7772cba8daf7b432a6e10f767dae27072
Aishwarya Vhatkar <iaishwaryavhatkar@gmail.com>
2024-01-25 11:21:15 -0800

    prototypal inheritance

--- prototypal inheritance/example using ES5 class.js
+++ prototypal inheritance/example using ES5 class.js
@@ -30,6 +30,9 @@ function Employee(name, age, title){
  * If you comment this line then you will get error (hit.greet is not a function) when you call empObj.greet()
  * So this line is basically inheriting Person's prototype properties into Employee class.
  * Here we inherited greet() method from Person class to Employee class and we didn't have to rewite the greet function
+ * 
+ * It sets the prototype of "Employee" constructor to a new object created by Person.prototype.
+ * This establishes a prototype chain where Employee inherits from Person
  */
 Employee.prototype = Object.create(Person.prototype);//sets up the prototype chain
 /**
@@ -38,6 +41,9 @@ Employee.prototype = Object.create(Person.prototype);//sets up the prototype cha
  * Since the assignment to Employee.prototype kills the existing Employee.prototype.constructor 
  * (which was set to Employee when the Employee constructor was created), we restore it.
  * Console and see the constructor of Employee object before and after below line(points to Employee class correctly)
+ * 
+ * It corrects the 'constructor' property of Employee 'prototype' to point back to 'Employee' constructor.
+ * This step is necessary because the previous line modified  the prototype
  */
 Employee.prototype.constructor = Employee;
 
@@ -45,12 +51,12 @@ const empObj = new Employee('Rohit', 32, 'Lead Engineer');
 console.log(empObj);//Employee {name: 'Rohit', age: 32, title: 'Lead Engineer'}
 console.log(empObj.greet());//Hello Rohit
 
-console.log('empObj instanceof Person', empObj instanceof Person);
-console.log('empObj instanceof Employee', empObj instanceof Employee);
-console.log('aishObj instanceof Person', aishObj instanceof Person);
-console.log('aishObj instanceof Employee', aishObj instanceof Employee);
-console.log('aishObj instanceof Object', aishObj instanceof Object);
-console.log('empObj instanceof Object', empObj instanceof Object);
+console.log('empObj instanceof Person', empObj instanceof Person);//true
+console.log('empObj instanceof Employee', empObj instanceof Employee);//true
+console.log('aishObj instanceof Person', aishObj instanceof Person);//true
+console.log('aishObj instanceof Employee', aishObj instanceof Employee);//false
+console.log('aishObj instanceof Object', aishObj instanceof Object);//true
+console.log('empObj instanceof Object', empObj instanceof Object);//true
 
 /**
  * Here are some things we can do with the constructor property that wouldn't have been possible had we not preserved it
